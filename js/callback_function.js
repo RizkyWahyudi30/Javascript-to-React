3f21ffeeefde9ae3702ba069c5cfaefbbd1126e5
RizkyWahyudi30 <rizkywahyudi302008@gmail.com>
2026-01-15 10:02:57 +0700

    callback function

--- js/callback_function.js
+++ js/callback_function.js
@@ -226,3 +226,222 @@ function setState(newState, callback) {
 setState({ count: 1 }, ({ count }) => {
   console.log(`New state: ${count}`);
 });
+
+// bentuk yang lainnya bisa seperti ini :
+// function setState(newState, callback) {
+//   console.log("State updated");
+//   callback(newState);
+// }
+
+// setState({ count: 1 }, (state) => {
+//   console.log(`New state: `, state);
+// });
+// output nya : New state: { count: 1 }
+
+// Soal lagi
+// Latihan soal 2
+
+// soal 1. callback formater
+function transformText(text, callback) {
+  callback(text);
+}
+
+transformText("Hello world", (text) => {
+  console.log(text.toUpperCase());
+});
+
+transformText("Hello World", (text) => {
+  console.log(text.toLowerCase());
+});
+
+transformText("Hello WORLD", (text) => {
+  console.log(text.split(" "));
+});
+
+// soal 2. callback validator
+function validateUser(user, onSuccess, onError) {
+  if (user.username.length >= 5 && user.age >= 18) {
+    onSuccess(user);
+  } else {
+    onError(user);
+  }
+}
+
+validateUser(
+  { username: "Shintya", age: 30 },
+  (user) => {
+    console.log("Berhasil:", user.username, user.age);
+  },
+  (user) => {
+    console.log("Gagal");
+  }
+);
+
+// soal 3. Callback data processor
+function processNumber(number, callback) {
+  callback(number);
+}
+
+function dikali(number) {
+  console.log(number.reduce((total, angka) => total * angka, 1));
+}
+
+function dikuadratkan(number) {
+  console.log(number.map((num) => num ** 2));
+}
+
+function difilter(number) {
+  console.log(number.filter((num) => (num % 2 === 0 ? num : null)));
+}
+
+const dataArrNum = [1, 2, 3, 4, 5];
+
+processNumber(dataArrNum, dikali);
+processNumber(dataArrNum, dikuadratkan);
+processNumber(dataArrNum, difilter);
+
+// soal 4. Callback control flow
+function runSequence(callback1, callback2, callback3) {
+  callback1();
+  callback2();
+  callback3();
+  console.log("Langkah selesai");
+}
+
+runSequence(
+  () => {
+    console.log("Langkah 1: dijalankan");
+  },
+  () => {
+    console.log("Langkah 2: dijalankan");
+  },
+  () => {
+    console.log("Langkah 3: dijalankan");
+  }
+);
+
+// soal 5. Callback Error Handling
+function safeDivide(a, b, callErr, callSucc) {
+  if (b === 0) {
+    callErr(a, b);
+  } else {
+    callSucc(a, b);
+  }
+}
+
+safeDivide(
+  10,
+  0,
+  () => {
+    console.log("Error");
+  },
+  (value) => {
+    console.log(value);
+  }
+);
+
+// soal 6. Callback Object Modifier
+function updateProfile(user, callback) {
+  const updateUser = callback(user);
+  return updateUser;
+}
+
+const updateData = updateProfile(
+  { id: 1, username: "rizal", age: 30 },
+  (value) => {
+    return {
+      ...value,
+      age: value.age + 1,
+    };
+  }
+);
+
+console.log(updateData);
+
+// soal 7. Callback Array Mapper
+function renderList(data, callback) {
+  return data.map(callback);
+}
+
+const dataList = (list, index) => `index-${index} | data: ${list}`;
+
+// jika data nya array
+const dataArr = ["satu", "dua", "tiga", "empat", "lima"];
+console.log(renderList(dataArr, dataList));
+
+// jika data array of objects
+
+// bisa seperit ini
+const dataListObj1 = (data) => `${data.name} - ${data.role}`;
+
+// atau dengan destructuring
+const dataListObj2 = ({ name, role }) => `${name} - ${role}`;
+
+const dataArrObj = [
+  { id: 1, name: "Raja Ayam", role: "Siswa" },
+  { id: 2, name: "Ikan Lele", role: "Guru" },
+];
+
+console.log(renderList(dataArrObj, dataListObj1));
+console.log(renderList(dataArrObj, dataListObj2));
+
+// soal 8. Callback async simulation
+function fetchData(data, callback) {
+  console.log("Menghubungi server...");
+
+  setTimeout(() => {
+    callback(data);
+  }, 3000);
+}
+
+fetchData({ id: 1, username: "kink" }, (value) => {
+  console.log("Data:", value);
+});
+
+// soal 9. Callback State Updater
+function setState(prevState, callback) {
+  return callback(prevState);
+}
+
+// bisa seperti ini
+const stateBaru = setState({ number: 2 }, ({ number }) => {
+  return {
+    number: number + 2,
+  };
+});
+console.log(stateBaru);
+
+// seperti ini juga
+const stateBaru2 = setState({ text: "halo", name: "Gemini" }, (prev) => ({
+  ...prev,
+  text: prev.text.toUpperCase(),
+}));
+console.log(stateBaru2);
+
+// dan juga seperti ini
+const hasil = setState(50, (number) => number * 100);
+console.log(hasil);
+
+// soal 10. Callback Composition
+function pipeLine(data, callback) {
+  // hasil sementara disimpan di sebuah variable
+  let resultData = data;
+
+  // perulangan untuk menjalankan callback satu per satu
+  for (let i = 0; i < callback.length; i++) {
+    // menjalankan fungsi saat ini dengan input hasil sebelumnya
+    resultData = callback[i](resultData);
+  }
+
+  // kirim hasil akhirnya
+  return resultData;
+}
+
+const diTambah = (n) => n + 5;
+const diBagi = (n) => n / 3;
+const diKali = (n) => n * 2;
+const diSisabagi = (n) => n % 3;
+const formatHasil = (n) => `Rp${n}`;
+
+const hasilAkhir = pipeLine(541, [diTambah, diBagi, diKali, diSisabagi]);
+console.log(hasilAkhir);
